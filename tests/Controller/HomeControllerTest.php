de92d0921eb79095c99f35407b4ab2d21bb2bfa5
Kévin PERQUY <kevin.perquy@yahoo.fr>
2023-11-29 18:31:35 +0100

    Test da la page d'accueil et des pages categories

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -4,12 +4,35 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class HomeControllerTest extends WebTestCase {
+class HomeControllerTest extends WebTestCase
+{
 
-    public function testIndex() {
-       $client = static::createClient();
+    public function testIndex()
+    {
+        $client = static::createClient();
         $client->request('GET', '/');
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         echo $client->getResponse()->getContent();
+    }
+
+    /**
+     * @dataProvider categoryProvider
+     */
+    public function testCatalogCategorie_WithProvider($category, $expected) {
+        $client = static::createClient();
+        $client->request('GET', "/catalog/".$category);
+        $this->assertSame($expected, $client->getResponse()->getStatusCode());
+    }
+
+    public function categoryProvider() {
+        return [
+            ["Alimentation", 200],
+            ["Électronique", 200],
+            ["Vêtements et mode", 200],
+            ["Beauté et santé", 200],
+            ["Sports et loisirs", 200],
+            ["Livres et papeterie", 200],
+            ["Automobile", 302] // Catégorie non présente dans le catalogue, redirection vers page d'accueil
+        ];
     }
 }
