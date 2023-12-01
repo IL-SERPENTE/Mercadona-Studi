d1c27252eb3f4eeca26dfbd64e898fcd0a8f9b53
Kévin PERQUY <kevin.perquy@yahoo.fr>
2023-12-01 13:52:20 +0100

    Test de l'entité poduct

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -25,25 +25,6 @@ class ProductTest extends TestCase {
         $this->assertSame($expected[4], $product->getCategorie());
     }
 
-    /**
-     * @dataProvider productSpecError_DataProvider
-     */
-    public function testGetSpecProductNotIdentical($spec,$expected)
-    {
-        $product = new Product();
-        $product->setLibelle($spec[0]);
-        $product->setDescription($spec[1]);
-        $product->setImage($spec[2]);
-        $product->setPrix($spec[3]);
-        $product->setCategorie($spec[4]);
-
-        $this->assertNotSame($expected[0], $product->getLibelle());
-        $this->assertNotSame($expected[1], $product->getDescription());
-        $this->assertNotSame($expected[2], $product->getImage());
-        $this->assertNotEquals($expected[3], $product->getPrix());
-        $this->assertNotSame($expected[4], $product->getCategorie());
-    }
-
     public function productSpec_DataProvider()
     {
         return [
@@ -58,20 +39,6 @@ class ProductTest extends TestCase {
             [
                 ["Téléviseur", "Plongez dans des films, programmes de télévision de qualité", "Téléviseur-oled.jpeg", 4999.99, "Électronique"],
                 ["Téléviseur", "Plongez dans des films, programmes de télévision de qualité", "Téléviseur-oled.jpeg", 4999.99, "Électronique"]
-            ]
-        ];
-    }
-
-    public function productSpecError_DataProvider()
-    {
-        return [
-            [
-                ["Brinsingr", "Un livre d'aventure et de fantaisie volume 3", "Brinsingr.png", 12.90, "Livre et papeterie"],
-                ["Brinsinger", "Un livre d'aventure et de fantaisie volume 2", "Brinsingr.jpg", 12, "Livr et papeterie"]
-            ],
-            [
-                ["Télévisueur OLED", "Plongez dans des films, programmes de télévision de qualité", "Téléviseur.png", 6999.99, "Électronique"],
-                ["Téléviseur", "Plongez dans des films, émissions sportives et jeux", "Télévisueur.jpg", 7999.99, "Électroniqu"]
             ]
         ];
     }
