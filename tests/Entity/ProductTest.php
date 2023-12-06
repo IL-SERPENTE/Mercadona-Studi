6d35c9727a06bc2c37248eeb2a7226d17aed8833
Kévin PERQUY <kevin.perquy@yahoo.fr>
2023-12-06 11:33:09 +0100

    Ajout d'un test

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -11,20 +11,20 @@ class ProductTest extends TestCase {
     /**
      * @dataProvider productSpec_DataProvider
      */
-    public function testGetSpecification_WithProvider($spec, $expected) {
+    public function testGetSpecification_WithProvider($libelle, $desc, $image, $prix, $category) {
         $product = new Product();
         $product
-            ->setLibelle($spec[0])
-            ->setDescription($spec[1])
-            ->setImage($spec[2])
-            ->setPrix($spec[3])
-            ->setCategorie($spec[4]);
+            ->setLibelle($libelle)
+            ->setDescription($desc)
+            ->setImage($image)
+            ->setPrix($prix)
+            ->setCategorie($category);
 
-        $this->assertSame($expected[0], $product->getLibelle());
-        $this->assertSame($expected[1], $product->getDescription());
-        $this->assertSame($expected[2], $product->getImage());
-        $this->assertEquals($expected[3], $product->getPrix());
-        $this->assertSame($expected[4], $product->getCategorie());
+        $this->assertSame($libelle, $product->getLibelle());
+        $this->assertSame($desc, $product->getDescription());
+        $this->assertSame($image, $product->getImage());
+        $this->assertEquals($prix, $product->getPrix());
+        $this->assertSame($category, $product->getCategorie());
     }
 
     public function testGetPromotion()
@@ -51,18 +51,9 @@ class ProductTest extends TestCase {
     public function productSpec_DataProvider()
     {
         return [
-            [
-                ["Eragon", "Un livre d'aventure et de fantaisie", "Eragon.jpg", 12.90, "Livre et papeterie"],
-                ["Eragon", "Un livre d'aventure et de fantaisie", "Eragon.jpg", 12.90, "Livre et papeterie"]
-            ],
-            [
-                ["L'aîné", "Un livre d'aventure et de fantaisie volume 2", "L'aîné.png", 12.90, "Livre et papeterie"],
-                ["L'aîné", "Un livre d'aventure et de fantaisie volume 2", "L'aîné.png", 12.90, "Livre et papeterie"]
-            ],
-            [
-                ["Téléviseur", "Plongez dans des films, programmes de télévision de qualité", "Téléviseur-oled.jpeg", 4999.99, "Électronique"],
-                ["Téléviseur", "Plongez dans des films, programmes de télévision de qualité", "Téléviseur-oled.jpeg", 4999.99, "Électronique"]
-            ]
+            ["Eragon", "Un livre d'aventure et de fantaisie", "Eragon.jpg", 12.90, "Livre et papeterie"],
+            ["L'aîné", "Un livre d'aventure et de fantaisie volume 2", "L'aîné.png", 12.90, "Livre et papeterie"],
+            ["Téléviseur", "Plongez dans des films, programmes de télévision de qualité", "Téléviseur-oled.jpeg", 4999.99, "Électronique"]
         ];
     }
 }
