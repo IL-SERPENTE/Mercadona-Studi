2654f4759370cb896371005824bd88f2ec05dc62
Kévin PERQUY <kevin.perquy@yahoo.fr>
2023-12-06 11:19:26 +0100

    Amélioration du code

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Product;
+use App\Entity\Promotion;
 use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase {
@@ -12,17 +13,39 @@ class ProductTest extends TestCase {
      */
     public function testGetSpecification_WithProvider($spec, $expected) {
         $product = new Product();
-        $product->setLibelle($spec[0]);
-        $product->setDescription($spec[1]);
-        $product->setImage($spec[2]);
-        $product->setPrix($spec[3]);
-        $product->setCategorie($spec[4]);
+        $product
+            ->setLibelle($spec[0])
+            ->setDescription($spec[1])
+            ->setImage($spec[2])
+            ->setPrix($spec[3])
+            ->setCategorie($spec[4]);
 
         $this->assertSame($expected[0], $product->getLibelle());
         $this->assertSame($expected[1], $product->getDescription());
         $this->assertSame($expected[2], $product->getImage());
         $this->assertEquals($expected[3], $product->getPrix());
         $this->assertSame($expected[4], $product->getCategorie());
+    }
+
+    public function testGetPromotion()
+    {
+        $promotion = $this->createMock(Promotion::class);
+
+        $product = new Product();
+        $product
+            ->setLibelle("PC Portable")
+            ->setDescription("PC conçus pour le jeux")
+            ->setPrix(1999.99)
+            ->setImage("Ordi-game.png")
+            ->setCategorie("Électronique")
+            ->setPromotion($promotion);
+
+        $this->assertSame("PC Portable", $product->getLibelle());
+        $this->assertSame("PC conçus pour le jeux", $product->getDescription());
+        $this->assertEquals(1999.99, $product->getPrix());
+        $this->assertSame("Ordi-game.png", $product->getImage());
+        $this->assertSame("Électronique", $product->getCategorie());
+        $this->assertSame($promotion, $product->getPromotion());
     }
 
     public function productSpec_DataProvider()
